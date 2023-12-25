a030a6a830976d3414090b6febcf37ab5acaf842
abdisudiatmika <sudiatmika.abdi@gmail.com>
2023-12-25 11:37:50 +0800

    first commit

---
+++ src/Applications/use_case/_test/AddThreadUseCase.test.js
@@ -0,0 +1,61 @@
+const ThreadRepository = require("../../../Domains/threads/ThreadRepository");
+const AddThreadUseCase = require("../AddThreadUseCase");
+
+describe("AddThreadUseCase", () => {
+  it("should orchestrating the add thread action correctly", async () => {
+    /**
+     * @TODO 3
+     * Lengkapi pengujian `AddThreadUseCase` agar dapat memastikan
+     * flow/logika yang dituliskan pada `AddThreadUseCase` benar!
+     *
+     * Tentunya, di sini Anda harus melakukan Test Double
+     * untuk memalsukan implmentasi fungsi `threadRepository`.
+     */
+
+    // Arrange
+    const useCasePayload = {
+      title: " Title",
+      body: " Body",
+      owner: "123",
+    };
+
+    const mockAddedThread = new AddedThread({
+      id: "thread-123",
+      title: useCasePayload.title,
+      owner: useCasePayload.owner,
+    });
+
+    /** creating dependency of use case */
+    const mockThreadRepository = new ThreadRepository();
+
+    /** mocking needed function */
+    mockThreadRepository.addThread = jest
+      .fn()
+      .mockImplementation(() => Promise.resolve(mockAddedThread));
+
+    /** creating use case instance */
+    const getThreadUseCase = new AddThreadUseCase({
+      threadRepository: mockThreadRepository,
+    });
+
+    // Action
+    const addedThread = await getThreadUseCase.execute(useCasePayload);
+
+    // Assert
+    expect(addedThread).toStrictEqual(
+      new AddedThread({
+        id: "thread-123",
+        title: useCasePayload.title,
+        owner: useCasePayload.owner,
+      })
+    );
+
+    expect(mockThreadRepository.addThread).toBeCalledWith(
+      new AddThread({
+        title: useCasePayload.title,
+        body: useCasePayload.body,
+        owner: useCasePayload.owner,
+      })
+    );
+  });
+});
