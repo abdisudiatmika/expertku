3297822e948d6b4c499f2d6b582da933997804da
abdisudiatmika <sudiatmika.abdi@gmail.com>
2023-12-26 15:33:17 +0800

    perbaikan testing

--- src/Infrastructures/http/createServer.js
+++ src/Infrastructures/http/createServer.js
@@ -55,13 +55,7 @@ const createServer = async (container) => {
       options: { container },
     },
   ]);
-  server.route({
-    method: "GET",
-    path: "/",
-    handler: () => ({
-      value: "Hello world!",
-    }),
-  });
+
   server.ext("onPreResponse", (request, h) => {
     // mendapatkan konteks response dari request
     const { response } = request;
